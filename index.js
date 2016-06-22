8935f185e15f9d5b946facee3929438d4faacec2
Ian Reid <ian@redscotch.com>
2016-06-22 01:03:20 -0700

    Added events

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
     is:require('arc-is'),
     array:require('arc-array'),
     object:require('arc-object'),
-    check:require('arc-check')
+    check:require('arc-check'),
+    events:require('arc-events')
 };
