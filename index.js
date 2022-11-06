616aca47eac0c762b996854d8ae686b9d031dcd1
anyuzer <ian@anyuzer.com>
2022-11-06 01:23:30 -0800

    oops, forgot an import

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import is from 'arc-is';
 import ArcArray from "arc-array";
 import ArcCheck from "arc-check";
 import ArcDate from "arc-date";
+import ArcEvents from "arc-events";
 import ArcHash from "arc-hash";
 import ArcObject from "arc-object";
 import ArcRegExp from "arc-reg-exp";
@@ -9,9 +10,9 @@ import ArcRouter from "arc-router";
 import ArcScheduler from "arc-scheduler";
 
 export {
-    is, ArcArray, ArcCheck, ArcDate, ArcHash, ArcObject, ArcRegExp, ArcRouter, ArcScheduler
+    is, ArcArray, ArcCheck, ArcDate, ArcEvents, ArcHash, ArcObject, ArcRegExp, ArcRouter, ArcScheduler
 }
 
 export default {
-    is, ArcArray, ArcCheck, ArcDate, ArcHash, ArcObject, ArcRegExp, ArcRouter, ArcScheduler
+    is, ArcArray, ArcCheck, ArcDate, ArcEvents, ArcHash, ArcObject, ArcRegExp, ArcRouter, ArcScheduler
 }
